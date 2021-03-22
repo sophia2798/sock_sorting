df501fad9921407717e9bf39d113c17a4a166e6f
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2021-03-21 22:26:18 -0700

    initalize second background sub method script, add starter code

---
+++ background_sub2.py
@@ -0,0 +1,31 @@
+import os
+import cv2
+from matplotlib import pyplot as plt
+
+'''
+For some reason, Jupyter Notebook/SageMaker doesn't like the cv.imshow() method. It seems to freeze the notebook and kill the kernel. I found a solution online that basically uses the matplotlib module's pyplot function to display the desired image(s) on a graph.
+'''
+
+test_video_name = 'sock_example.avi'
+
+cap = cv2.VideoCapture(test_video_name)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+while(1):
+    ret, frame = cap.read()
+    fgmask = fgbg.apply(frame)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+    
+    plt.imshow(fgmask)
+    plt.show()
+
+# the code below is what you could use if cv2.imshow() function works in your python IDE
+#     cv2.imshow('frame', fgmask)
+#     k = cv2.waitKey(30) & 0xff
+#     if k == 27:
+#         break
+        
+cap.release()
+cv2.destroyAllWindows()
