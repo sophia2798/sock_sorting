5cc60eef2ed0ff87f0b8920d6edb7dbc09732263
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2021-03-25 22:51:17 -0700

    run sigcontour function on frames conditionally if contours exist

--- background_sub2.py
+++ background_sub2.py
@@ -5,6 +5,8 @@ import numpy as np
 
 '''
 For some reason, Jupyter Notebook/SageMaker doesn't like the cv.imshow() method. It seems to freeze the notebook and kill the kernel. I found a solution online that basically uses the matplotlib module's pyplot function to display the desired image(s) on a graph.
+
+It should be noted that images are loaded in BGR by default, so you will want to convert them to RGB using cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 '''
 
 test_video_name = 'sock_example.avi'
@@ -39,9 +41,20 @@ while(1):
     ret, frame = cap.read()
     fgmask = fgbg.apply(frame)
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-    
-    # the image is loaded in BGR by default, want to convert it to RGB
-    plt.imshow(cv2.cvtColor(fgmask, cv2.COLOR_BGR2RGB))
+
+    # use canny edge detection
+    edges = cv2.Canny(fgmask, 0, 100, apertureSize=3)
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # conditional to check if there are any contours in the frame
+    if hierarhcy is None:
+        plt.imshow(cv2.cvtColor(fgmask, cv2.COLOR_BGR2RGB))
+    else:
+        sigcontour = findSignificantContour(edges)
+        copy = np.copy(fgmask)
+        cv2.drawContours(copy, sigcontour, -1, (0,255,0), 2, cv2.LINE_AA, maxLevel=1)
+        plt.imshow(cv2.cvtColor(copy, cv2.COLOR_BGR2RGB))
+
     plt.show()
 
 # the code below is what you could use if cv2.imshow() function works in your python IDE
