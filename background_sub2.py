0f77055ef7f9bfa3316e46463ee341e7eb9bdd10
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2021-03-25 22:45:54 -0700

    add sigcontour function to background_sub2

--- background_sub2.py
+++ background_sub2.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 from matplotlib import pyplot as plt
+import numpy as np
 
 '''
 For some reason, Jupyter Notebook/SageMaker doesn't like the cv.imshow() method. It seems to freeze the notebook and kill the kernel. I found a solution online that basically uses the matplotlib module's pyplot function to display the desired image(s) on a graph.
@@ -12,6 +13,27 @@ cap = cv2.VideoCapture(test_video_name)
 
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
 fgbg = cv2.createBackgroundSubtractorMOG2()
+
+# same function from preprocessing_test.py
+def findSignificantContour(edgeImg):
+    contours, hierarchy = cv2.findContours(edgeImg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    level1Meta = []
+    for contourIndex, tupl in enumerate(hierarhcy[0]):
+        if tupl[3] == -1:
+            tupl = np.insert(tupl.copy(), 0, [contourIndex])
+            level1Meta.append(tupl)
+
+    contoursWithArea = []
+    for tupl in level1Meta:
+        contourIndex = tupl[0]
+        contour = contours[contourIndex]
+        area = cv2.contourArea(contour)
+        contoursWithArea.append([contour, area, contourIndex])
+
+    contoursWithArea.sort(key=lambda meta: meta[1], reverse=True)
+    largestContour = contoursWithArea[0][0]
+    return largestContour
 
 while(1):
     ret, frame = cap.read()
@@ -29,4 +51,4 @@ while(1):
 #         break
         
 cap.release()
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
